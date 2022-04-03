f808c5f33091a2dc45f3fc6414f248e55725f8b1
avramenkomy <mihail.avramenko@ocrv.ru>
2022-04-03 18:24:58 +0300

    WIP: connect app to project

--- altika_backend/altika_backend/settings.py
+++ altika_backend/altika_backend/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # library
+    'rest_framework',
+    # my_apps
+    'altika',
 ]
 
 MIDDLEWARE = [
